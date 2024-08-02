bd680bc65c3eb4e400bb149b6551adb2eb4e5f08
TKNA <Brodyjeanes1@gmail.com>
2024-08-02 18:16:33 +0800

    Created main window frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from pytube import YouTube 
 from PIL import Image, ImageTk
+from settings import *
 
 try: # ONLY WORKS ON WINDOWS SO EXCEPTION HANDLING FOR OTHER OS SYSTEMS
     from ctypes import windll, byref, sizeof, c_int # required to change title bar colour
@@ -18,6 +19,42 @@ class App(ctk.CTk):
         self.resizable(False,False)
         self.config(background = "#374A67")
 
+        # changes the title bar colour
+        self.changeTitleBarColour()
+
+        # Gridded Layout
+        self.columnconfigure(0, weight = 1)
+        self.rowconfigure(0, weight = 1)
+        self.rowconfigure(1, weight = 2)
+
+        TopFrame(self)
+
+
         self.mainloop()
+
+        # Changes the colour of the title bar
+    def changeTitleBarColour(self):
+        try: # ONLY WORKS ON WINDOWS SO EXCEPTION HANDLING FOR OTHER OS SYSTEMS
+            # Change title bar colour
+            HWND = windll.user32.GetParent(self.winfo_id())
+            windll.dwmapi.DwmSetWindowAttribute(HWND, 35, byref(c_int(titleBarHexColour)), sizeof(c_int)) # 35 = title bar colour attribute
+        except:
+            pass
+
+
+class TopFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        # text attributes
+        headerFont = ctk.CTkFont(family = font, size = headerTextSize, weight = "bold")
+        super().__init__(master = parent, corner_radius = buttonCornerRadius)
+        self.grid(row = 0, column = 0, sticky = "nsew", padx = 10, pady = 10)
+
+        header = ctk.CTkLabel(
+            master = self,
+            text = "YouTube Video Downloader", 
+            font = headerFont, 
+            text_color = white)
+        header.pack(fill = "both", expand = True, padx = 5, pady = 5)
+
 
 App()
---
+++ settings.py
@@ -0,0 +1,17 @@
+# Text attributes
+font = "Calibri"
+headerTextSize = 50
+textSize = 26
+buttonCornerRadius = 20
+
+# Colours - Subject to change
+green = "#50BFAB"
+darkGreen = "#3A8A7B"
+white = "#F2F2F2"
+black = "#1F1F1F"
+lightGray = "#E8E8E8"
+gray = "#D9D9D9"
+
+#374A67 same blue as the background
+# title bar hex colour
+titleBarHexColour = 0x00674A37
